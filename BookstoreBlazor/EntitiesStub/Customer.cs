4cc4bcc266125c9c5a12101ab4db47b6e8373ba8
Gabriel Comstock <comstogm@mail.uc.edu>
2023-11-08 16:06:43 -0800

    Actually add ShoppingCartService

---
+++ BookstoreBlazor/EntitiesStub/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment4Lib_Comstock_Gabriel.Entities
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        [DisplayName("First Name")]
+        public string? FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        [DisplayName("Zip Code")]
+        public string? PostalCode { get; set; }
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
---
+++ BookstoreBlazor/Services/CartService.cs
@@ -0,0 +1,26 @@
+﻿using Assignment4Lib_Comstock_Gabriel.Entities;
+
+namespace BookstoreBlazor.Services
+{
+    public class CartService
+    {
+        //List of ints to hold item IDs
+        public List<int> Products { get; set; } = new();
+
+        public void AddProductToCart(int productId) 
+        {
+            Products.Add(productId);
+        }
+
+        public List<int> GetProductsFromCart()
+        {
+            return Products;
+        }
+
+        public void RemoveProductFromCart(int productId)
+        {
+            //todo
+        }
+
+    }
+}
