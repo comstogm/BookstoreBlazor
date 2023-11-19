8fd6bb5d6bedf919ed4716a88e4299bd94be1db8
Robert Brown jr <rob.jr24@yahoo.com>
2023-11-19 17:03:16 -0500

    Added dbcontext

--- BookstoreBlazor/Program.cs
+++ BookstoreBlazor/Program.cs
@@ -1,5 +1,6 @@
 using BookstoreBlazor;
 using BookstoreBlazor.Services;
+using BookstoreDatabase.Data;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +10,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<CartService>();
+
+builder.Services.AddDbContext<BookstoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString(BookstoreDB));
+});
 
 await builder.Build().RunAsync();
