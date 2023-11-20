638d0b50a76ff1af84eed923e8dfb953e6fa15e9
Gabriel Comstock <comstogm@mail.uc.edu>
2023-11-19 17:11:32 -0800

    Does not need direct access to SQL

--- BookstoreBlazor/Program.cs
+++ BookstoreBlazor/Program.cs
@@ -11,9 +11,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<CartService>();
 
-builder.Services.AddDbContext<BookstoreContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString(BookstoreDB));
-});
-
 await builder.Build().RunAsync();
