dcca9653aa73b7ad56943b5ca3ef4845aa1c91f4
Gabriel Comstock <comstogm@mail.uc.edu>
2023-11-24 12:35:38 -0800

    ShoppingCart total, try to submit order

--- BookstoreBlazor/Services/CartService.cs
+++ BookstoreBlazor/Services/CartService.cs
@@ -4,7 +4,6 @@ namespace BookstoreBlazor.Services
 {
     public class CartService
     {
-        //List of ints to hold item IDs
         public List<Book> Products { get; set; } = new();
 
         public void AddProductToCart(Book book) 
--- BookstoreDatabase/Entitites/OrderLineItem.cs
+++ BookstoreDatabase/Entitites/OrderLineItem.cs
@@ -5,6 +5,13 @@ namespace BookstoreDatabase.Entitites
     [Table("OrderLineItems")]
     public class OrderLineItem
     {
+        public OrderLineItem() { }
+        public OrderLineItem(int bookId, Book? bookName)
+        {
+            BookId = bookId;
+            BookName = bookName;
+        }
+
         public int OrderLineItemId { get; set; }
 
         public int OrderId { get; set; }
